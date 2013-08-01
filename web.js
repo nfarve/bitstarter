ff9321fc6d65dbcf7609c79374468fe1e491c080
Niaja <nfarve@mit.edu>
2013-08-01 23:54:14 +0000

    trying to fix web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html', 'utf8');
+    var output = fs.readFileSync('index.html');
+    var string = output.toString; 
+    response.send(string);
 });
 
 var port = process.env.PORT || 5000;
